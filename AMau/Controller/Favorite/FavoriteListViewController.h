98f2480f74224e0e2a869ecc5f535a3af474dcab
KemingChen <believe75467@gmail.com>
2015-06-22 12:55:34 +0800

    finish Layout FavoriteListView

---
+++ AMau/Controller/Favorite/FavoriteListViewController.h
@@ -0,0 +1,13 @@
+//
+//  FavoriteListViewController.h
+//  AMau
+//
+//  Created by Keming on 2015/6/22.
+//  Copyright (c) 2015年 NTUT-IOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FavoriteListViewController : UIViewController
+
+@end
